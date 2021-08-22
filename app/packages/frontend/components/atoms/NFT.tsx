226d57014ab740caa8ac4c3ad3f8891011e84804
jjperezaguinaga <me@jjperezaguinaga.com>
2021-08-22 16:06:08 +0000

    feat: Fixes #17 - Queried real owner when wrapper contract found

--- app/packages/frontend/components/atoms/NFT.tsx
+++ app/packages/frontend/components/atoms/NFT.tsx
@@ -1,9 +1,22 @@
-import { Box, Text, Image, Skeleton, Flex, Link, useColorMode } from '@chakra-ui/react'
+import {
+  Box,
+  Text,
+  Image,
+  Skeleton,
+  Flex,
+  Link,
+  useColorMode,
+} from '@chakra-ui/react'
+import TransferArt from '../../artifacts/contracts/TransferArt.sol/TransferArt.json'
 import { useNft } from 'use-nft'
 import { ENS } from './ENS'
 import { useOwner } from '../../lib/hooks'
-import { DEFAULT_CHAIN_ID } from '../../lib/constants'
-import { getExplorerAddressLink } from '@usedapp/core'
+import { DEFAULT_CHAIN_ID, WRAPPED_TOKEN_CONTRACT } from '../../lib/constants'
+import { getExplorerAddressLink, useEthers } from '@usedapp/core'
+import { useEffect, useState } from 'react'
+import { TransferArt as TransferArtType } from '../../types/typechain'
+import { getCurrentProvider } from '../../lib/connectors'
+import { Contract } from 'ethers'
 
 export const Nft = ({
   address,
@@ -12,10 +25,30 @@ export const Nft = ({
   address: string
   tokenId: string
 }) => {
+  const { library } = useEthers()
   const { loading, error, nft } = useNft(address, tokenId)
   const { colorMode } = useColorMode()
+  const [realOwner, setRealOwner] = useState('')
+  const provider = getCurrentProvider(library)
   const bgColor = { light: 'gray.50', dark: 'gray.600' }
   const owner = useOwner(tokenId)
+
+  useEffect(() => {
+    const loadRealOwner = async () => {
+      const realOwner =
+        owner == WRAPPED_TOKEN_CONTRACT
+          ? await (contract => contract.ownerOf(tokenId))(
+              new Contract(
+                WRAPPED_TOKEN_CONTRACT,
+                TransferArt.abi,
+                provider
+              ) as TransferArtType
+            )
+          : owner
+      setRealOwner(realOwner)
+    }
+    loadRealOwner()
+  }, [owner])
 
   // nft.loading is true during load.
   if (loading) return <Skeleton height="150px" />
@@ -44,7 +77,7 @@ export const Nft = ({
           {og ? `Token #${realId}` : `Copy of ${realId}`}
         </Text>
       </Flex>
-      
+
       <Box p="5" bg={bgColor[colorMode]} border="1px solid #ddd">
         <Image src={nft.image} alt={nft.name} />
       </Box>
@@ -52,7 +85,7 @@ export const Nft = ({
         Token Id #{tokenId}
       </Text>
       <Link href={getExplorerAddressLink(owner, DEFAULT_CHAIN_ID)} isExternal>
-        <ENS props={{ fontSize: 'xs' }} address={owner} />
+        <ENS props={{ fontSize: 'xs' }} address={realOwner} />
       </Link>
     </Flex>
   )
--- app/packages/frontend/lib/hooks.ts
+++ app/packages/frontend/lib/hooks.ts
@@ -4,15 +4,19 @@ import { useContractCall } from '@usedapp/core'
 import { Interface } from '@ethersproject/abi'
 import { Falsy } from '@usedapp/core/dist/esm/src/model/types'
 
-export function useOwner(tokenId: string | Falsy): string | undefined {
+export function ownerOf(tokenId: string | Falsy, contractAddress: string, abi: Interface): string | undefined {
   const [owner] =
     useContractCall(
       tokenId && {
-        abi: new Interface(TransferArt.abi),
-        address: TRANSFER_ART_CONTRACT_ADDRESS,
+        abi,
+        address: contractAddress,
         method: 'ownerOf',
         args: [tokenId],
       }
     ) ?? []
-  return owner
+  return owner;
+}
+
+export function useOwner(tokenId: string | Falsy): string | undefined {
+  return ownerOf(tokenId, TRANSFER_ART_CONTRACT_ADDRESS, new Interface(TransferArt.abi))
 }
