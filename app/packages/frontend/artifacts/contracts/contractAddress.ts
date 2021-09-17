25fd26945f36a9500dee91e643baddb040b26904
jjperezaguinaga <me@jjperezaguinaga.com>
2021-09-17 05:09:22 +0000

    feat: Adding auction house, updating minting status

--- app/packages/frontend/artifacts/contracts/contractAddress.ts
+++ app/packages/frontend/artifacts/contracts/contractAddress.ts
@@ -1,2 +1,3 @@
 export const TransferArt = '0x5d99371a4297dfd301a1f22eff22a7e0ed9b4482'
+export const AnimalColoringBook = '0x69c40e500b84660cb2ab09cb9614fa2387f95f64'
 export const MulticallContract = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
